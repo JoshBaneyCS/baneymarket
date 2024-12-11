01d540279d6a0cc2ca7053da8f32a38f2b0b344e
Joshua Baney <me@joshbaney.com>
2024-12-11 03:38:38 -0500

    Create About.js

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+// src/components/About.js
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>BaneyMarket is committed to providing the best stock trading experience.</p>
+    </div>
+  );
+};
+
+export default About;
